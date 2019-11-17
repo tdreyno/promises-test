2b85fc421abe1b8b905d4ac5b4266667d1e4d73c
Thomas Reynolds <me@tdreyno.com>
2019-11-16 22:05:49 -0800

    Use abstract

--- src/OurPromise.ts
+++ src/OurPromise.ts
@@ -3,13 +3,13 @@ export abstract class OurPromise<T> {
     fn: (resolver: (value: T) => void, rejecter: (error: any) => void) => void
   ) {}
 
-  then<R>(callback: (value?: T, error?: any) => R | ThisType<R>): ThisType<R> {
-    throw new Error("Must implement `then`");
-  }
+  abstract then<R>(
+    callback: (value?: T, error?: any) => R | ThisType<R>
+  ): ThisType<R>;
 
-  catch<R>(callback: (error: any) => T | R | ThisType<T | R>): ThisType<T | R> {
-    throw new Error("Must implement `catch`");
-  }
+  abstract catch<R>(
+    callback: (error: any) => T | R | ThisType<T | R>
+  ): ThisType<T | R>;
 
   static resolve<T>(val: T): ThisType<T> {
     throw new Error("Must implement static `resolve`");
